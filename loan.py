ade05bf177fbf2e949d112d15e9e41c02cb92773
Darshit165 <darshitp2211@gmail.com>
2026-09-23 15:17:40 +0530

    check outlier

--- loan.py
+++ loan.py
@@ -8,3 +8,28 @@ print(df.head())
 print(df.info())
 print(df.describe())
 print(df.shape)
+
+#check missing value 
+print("Missing values in each column:")
+print(df.isnull().sum())
+
+#check target variable distribution
+print("Target variable distribution:")
+print(df["loan_status"].unique())
+print(df['loan_status'].value_counts())
+
+#visualize target variable distribution
+sns.countplot(x='loan_status', data=df)
+plt.show()
+
+#Outlier check
+numeric_columns = df.select_dtypes(include=[np.number]).columns
+print("Numeric columns for outlier check:", numeric_columns)
+for col in numeric_columns:
+    plt.figure(figsize=(10, 5))
+    sns.boxplot(x=df[col])
+    plt.title(f'Boxplot of {col}')
+    plt.show()
+
+print((df["person_age"]>100).sum())
+print((df["person_emp_length"]>60).sum())
